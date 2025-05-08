0634f361288cf7d8825d559d3c4685834f7237cf
NguyenVung0408 <nguyenthanhvung0408@gmail.com>
2025-05-08 16:29:07 +0700

    Initial commit

---
+++ ShopeeClone/src/App.tsx
@@ -0,0 +1,10 @@
+import useRouteElements from "./useRouteElement"
+
+function App() {
+  const routeElements = useRouteElements() 
+  return (
+    <div>{routeElements}</div>
+  )
+}
+
+export default App
