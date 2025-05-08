d23f58cae59e519f79a41b9a3e7059aad4e2aa99
NguyenVung0408 <nguyenthanhvung0408@gmail.com>
2025-05-08 17:39:15 +0700

    push code update css

--- ShopeeClone/src/main.tsx
+++ ShopeeClone/src/main.tsx
@@ -1,13 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from 'src/App'
 import './index.css'
-import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+import 'src/i18n/i18n'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AppProvider } from './contexts/app.context'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0
+    }
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+)
